eda3c6cb08891db2aa7dc363b2b491edadbe7fbb
patrickarcher414 <patrickarcher414@gmail.com>
2022-07-02 12:46:49 -0500

    User model complete

--- models/User.js
+++ models/User.js
@@ -1,2 +1,44 @@
 const { Schema, model, Types } = require('mongoose');
 
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+      trim: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const Thought = model('User', UserSchema);
+
+module.exports = User;
