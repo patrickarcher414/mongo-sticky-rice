ca1fabbc99207a034dbbc36d1754e198780038eb
patrickarcher414 <patrickarcher414@gmail.com>
2022-07-03 13:06:42 -0500

    user model fixed

--- models/User.js
+++ models/User.js
@@ -43,6 +43,6 @@ UserSchema.virtual('friendCount').get(function() {
   return this.friends.length;
 });
 
-const Thought = model('User', UserSchema);
+const User = model('User', UserSchema);
 
 module.exports = User;
