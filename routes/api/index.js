70997325e87723a3fca81d8cfff259741961c7db
patrickarcher414 <patrickarcher414@gmail.com>
2022-07-03 13:34:08 -0500

    api routes index fixed

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>404 Error. Request not found</h1>')
+})
 
 module.exports = router;
