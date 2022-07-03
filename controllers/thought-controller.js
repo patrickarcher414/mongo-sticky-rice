392cfb99b57d3fbad0f705b93c1d3d02df41eeb7
patrickarcher414 <patrickarcher414@gmail.com>
2022-07-03 13:00:15 -0500

    thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,68 @@
-getAllThoughts,
-  getThoughtById,
-  addThought,
-  updateThought,
-  removeThought,
-  addReaction,
-  removeReaction
+const { Thought } = require('../models');
+ 
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({req})
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No Thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData)
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  addThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  // update thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  addReaction({ body }, res) {
+    Thought.friends.create(body)
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => res.json(err));
+  },
+
+  removeReaction({ params }, res) {
+    Thought.friends.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = thoughtController;
